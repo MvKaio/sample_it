f5bbdc008df2f176b81adc85736765ffc97f83de
gabrielmmf <gabrielmmfialho@hotmail.com>
2022-10-23 15:15:39 -0300

    Added option to edit collections

--- webapp/src/views/CollectionView.vue
+++ webapp/src/views/CollectionView.vue
@@ -45,6 +45,64 @@ async function deleteItem(index) {
 //Definindo a data de alteração da coleção
 var date = new Date();
 
+const editting = ref(false)
+
+const name_editted = ref('')
+const description_editted = ref('')
+const labels_editted = ref([])
+const label_editted = ref('')
+
+function startEdit() {
+    name_editted.value = collection.value.name
+    description_editted.value = collection.value.description
+    labels_editted.value = Array.from(collection.value.labels)
+    label_editted.value = ''
+    toggleEdit()
+}
+
+function cancelEdit() {
+    toggleEdit()
+}
+
+async function saveEdit() {
+    collection.value.name = name_editted.value
+    collection.value.description = description_editted.value
+    collection.value.labels = Array.from(labels_editted.value)
+    collection.value.updated_at = date
+
+    const dataJson = JSON.stringify(collection.value);
+    console.log(dataJson)
+
+    const req = await fetch(`http://localhost:3000/collections/${collectionID}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: dataJson
+    });
+    if (!req.ok) {
+        throw new Error(req.status);
+    }
+
+    toggleEdit()
+}
+
+function toggleEdit() {
+    editting.value = !editting.value
+    console.log(editting.value)
+}
+
+function addLabel() {
+
+    if (label_editted.value.length > 0 && !Array.from(labels_editted.value).includes(label_editted.value)) {
+        labels_editted.value.push(label_editted.value)
+    }
+    else {
+        if (Array.from(labels_editted.value).includes(label_editted.value)) {
+            alert("Label already exists")
+        }
+    }
+    label_editted.value = ''
+}
+
 </script>
                     
 <template>
@@ -56,11 +114,26 @@ var date = new Date();
         <div v-for="(item, index) in collection.items">{{item.name}}</div> -->
 
         <div class="text-center h-full overflow-auto">
-            <div class="h-[5%] flex items-center justify-center">
+            <div v-if="editting" class="h-[5%] flex items-center justify-center">
+                <h1 class="text-center text-2xl">Collection name: </h1>
+                <input type="text" class="field text-center mx-4" id="name_editted" v-model="name_editted"
+                    v-on:keydown.enter.prevent=''>
+            </div>
+            <div v-else class="h-[5%] flex items-center justify-center">
                 <h1 class="text-center text-2xl">Collection name: {{collection.name}}</h1>
 
             </div>
-            <div class="h-[30%] w-[100%] items-center justify-center">
+            <div v-if="editting" class="h-[30%] w-[100%] items-center justify-center">
+                <div class="h[20%] flex items-center justify-center">
+                    <h1 class="text-center text-2xl">Collection description: </h1>
+                </div>
+                <div class="h-[80%]">
+                    <textarea type="text" class="textarea_vanilla h-full w-full" id="desription_editted"
+                        v-model="description_editted"></textarea>
+                </div>
+
+            </div>
+            <div v-else class="h-[30%] w-[100%] items-center justify-center">
                 <div class="h[20%] flex items-center justify-center">
                     <h1 class="text-center text-2xl">Collection description: </h1>
                 </div>
@@ -82,8 +155,8 @@ var date = new Date();
                 <div class="h-[60%] black-bg overflow-auto">
                     <div class="grid grid-cols-3 mb-4 mx-[1%] space-x-[10%]" v-for="(item, index) in collection.items">
                         <div class="text-xl text-centerlink label-pure-black flex items-center justify-center">
-                            <router-link
-                                :to="{name: 'View Item', params: {collectionid: collection.id, itemid: index}}" class="link">
+                            <router-link :to="{name: 'View Item', params: {collectionid: collection.id, itemid: index}}"
+                                class="link">
                                 {{item.name}}
                             </router-link>
                         </div>
@@ -105,7 +178,35 @@ var date = new Date();
                 </div>
             </div>
 
-            <div class="h-[25%] w-[100%] items-center justify-center">
+            <div v-if="editting" class="h-[25%] w-full flex items-center justify-center">
+                <div class="h-full w-3/5">
+                    <div class="w-full flex items-center justify-center">
+                        <h1 class="text-center text-2xl">Collection Labels:</h1>
+                    </div>
+                    <div class="h-[80%] w-full black-bg overflow-auto">
+                        <div class=" mb-4 mx-[1%]" v-for="(lb, index) in labels_editted"><span
+                                class="label-pure-black">{{lb}}</span></div>
+                    </div>
+
+                </div>
+
+                <div class="h-full w-2/5 flex items-center justify-center space-x-[5%]">
+                    <input type="text" class="field w-3/5 text-center" id="label_editted" v-model="label_editted"
+                        v-on:keydown.enter.prevent='addLabel'>
+                    <input class="button w-[30%]" id="addLabelButton" type="button" value="Add Label"
+                        v-on:click="addLabel">
+
+                </div>
+
+            </div>
+
+            <!-- <div v-if="editting" class="h-[25%] w-[40%] inline-block items-center">
+                <input type="text" class="field w-3/5 text-center" id="label_editted" v-model="label_editted"
+                    v-on:keydown.enter.prevent='addLabel'>
+                <input class="button w-[38%]" id="addLabelButton" type="button" value="Add Label" v-on:click="addLabel">
+            </div> -->
+
+            <div v-else class="h-[25%] w-[100%] items-center justify-center">
                 <div class="h[20%] flex items-center justify-center">
                     <h1 class="text-center text-2xl">Collection Labels:</h1>
                 </div>
@@ -115,15 +216,20 @@ var date = new Date();
                 </div>
             </div>
 
-            <div class="space-x-4 h-[10%] flex items-center">
+            <div v-if="editting" class="space-x-4 h-[10%] flex items-center">
+                <div class="text-center w-full space-x-[1%]">
+                    <input type="button" class="button" value="Save" @click="saveEdit">
+                    <input type="button" class="button" value="Cancel" @click="cancelEdit">
+                </div>
+            </div>
+            <div v-else class="space-x-4 h-[10%] flex items-center">
                 <div class="text-center w-full space-x-[1%]">
                     <router-link :to="{name: 'Generate Sample', params: {id: collectionID}}"><input type="button"
                             class="button" value="Generate a New Sample"></router-link>
-                    <router-link :to="{name: 'EditCollection', params: {id: collectionID}}"><input type="button"
-                            class="button" value="Edit"></router-link>
                     <router-link :to="{name: 'Add Item', params: {id: collectionID}}"><input type="button"
                             class="button" value="Add Items"></router-link>
-                    <input type="button" class="button" value="Delete this collection" @click="deleteCollection">
+                    <input type="button" class="button" value="Edit Collection" @click="startEdit">
+                    <input type="button" class="button" value="Delete Collection" @click="deleteCollection">
                 </div>
             </div>
         </div>
