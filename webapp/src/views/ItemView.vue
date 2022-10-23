9e8bac79c4c0801268c876de5e2df44ce9ea6d99
gabrielmmf <gabrielmmfialho@hotmail.com>
2022-10-23 17:22:34 -0300

    Added transitions and animations on editting items

--- webapp/src/views/ItemView.vue
+++ webapp/src/views/ItemView.vue
@@ -105,91 +105,97 @@ var date = new Date();
     <div class="component-container">
 
         <div class="text-center h-full overflow-auto">
-            <div v-if="editting" class="h-[5%] flex items-center justify-center">
-                <h1 class="text-center text-2xl">Item name: {{item.name}}</h1>
-                <input type="text" class="field text-center mx-4" id="name_editted" v-model="name_editted"
-                    v-on:keydown.enter.prevent=''>
-            </div>
-            <div v-else class="h-[5%] flex items-center justify-center">
-                <h1 class="text-center text-2xl">Collection name: {{item.name}}</h1>
-            </div>
-            <div v-if="editting" class="h-[30%] w-[100%] items-center justify-center">
-                <div class="h[20%] flex items-center justify-center">
-                    <h1 class="text-center text-2xl">Item description: </h1>
+            <transition name="fade" mode="out-in">
+                <div v-if="editting" class="h-[5%] flex items-center justify-center">
+                    <h1 class="text-center text-2xl">Item name: {{item.name}}</h1>
+                    <input type="text" class="field text-center mx-4" id="name_editted" v-model="name_editted"
+                        v-on:keydown.enter.prevent=''>
                 </div>
-                <div class="h-[80%]">
-                    <textarea type="text" class="textarea_vanilla h-full w-full" id="desription_editted"
-                        v-model="description_editted"></textarea>
+                <div v-else class="h-[5%] flex items-center justify-center">
+                    <h1 class="text-center text-2xl">Collection name: {{item.name}}</h1>
+                </div>
+            </transition>
+            <transition name="fade" mode="out-in">
+                <div v-if="editting" class="h-[30%] w-[100%] items-center justify-center">
+                    <div class="h[20%] flex items-center justify-center">
+                        <h1 class="text-center text-2xl">Item description: </h1>
+                    </div>
+                    <div class="h-[80%]">
+                        <textarea type="text" class="textarea_vanilla h-full w-full" id="desription_editted"
+                            v-model="description_editted"></textarea>
+                    </div>
+
+                </div>
+                <div v-else class="h-[30%] w-[100%] items-center justify-center">
+                    <div class="h[20%] flex items-center justify-center">
+                        <h1 class="text-center text-2xl">Item description: </h1>
+                    </div>
+                    <div class="h-[80%] black-bg text-left word-break overflow-auto">
+                        <span>{{item.description}}</span>
+                    </div>
+
+                </div>
+            </transition>
+
+
+
+
+
+
+
+
+
+            <transition name="fade" mode="out-in">
+                <div v-if="editting" class="h-[55%] w-full flex items-center justify-center">
+                    <div class="h-full w-3/5">
+                        <div class="w-full flex items-center justify-center">
+                            <h1 class="text-center text-2xl">item Labels:</h1>
+                        </div>
+                        <div class="h-[80%] w-full black-bg overflow-auto">
+                            <div class=" mb-4 mx-[1%]" v-for="(lb, index) in labels_editted"><span
+                                    class="label-pure-black">{{lb}}</span>
+                                <font-awesome-icon icon="trash" class="link hover:text-red-600 mx-2"
+                                    @click="deleteItemLabel(index)" />
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="h-full w-2/5 flex items-center justify-center space-x-[5%]">
+                        <input type="text" class="field w-3/5 text-center" id="label_editted" v-model="label_editted"
+                            v-on:keydown.enter.prevent='addLabel'>
+                        <input class="button w-[30%]" id="addLabelButton" type="button" value="Add Label"
+                            v-on:click="addLabel">
+
+                    </div>
+
                 </div>
 
-            </div>
-            <div v-else class="h-[30%] w-[100%] items-center justify-center">
-                <div class="h[20%] flex items-center justify-center">
-                    <h1 class="text-center text-2xl">Item description: </h1>
-                </div>
-                <div class="h-[80%] black-bg text-left word-break overflow-auto">
-                    <span>{{item.description}}</span>
-                </div>
-
-            </div>
-
-
-
-
-
-
-
-
-
-
-            <div v-if="editting" class="h-[55%] w-full flex items-center justify-center">
-                <div class="h-full w-3/5">
-                    <div class="w-full flex items-center justify-center">
+                <div v-else class="h-[55%] w-[100%] items-center justify-center">
+                    <div class="flex items-center justify-center">
                         <h1 class="text-center text-2xl">item Labels:</h1>
                     </div>
-                    <div class="h-[80%] w-full black-bg overflow-auto">
-                        <div class=" mb-4 mx-[1%]" v-for="(lb, index) in labels_editted"><span
-                                class="label-pure-black">{{lb}}</span>
-                            <font-awesome-icon icon="trash" class="link hover:text-red-600 mx-2"
-                                @click="deleteItemLabel(index)" />
-                        </div>
+                    <div class="h-[80%] black-bg overflow-auto">
+                        <div class=" mb-4 mx-[1%]" v-for="(lb, index) in item.labels"><span
+                                class="label-pure-black">{{lb}}</span></div>
                     </div>
-
+                </div>
+            </transition>
+            <transition name="fade" mode="out-in">
+                <div v-if="editting" class="space-x-4 h-[10%] flex items-center">
+                    <div class="text-center w-full space-x-[1%]">
+                        <input type="button" class="button" value="Save" @click="saveEdit">
+                        <input type="button" class="button" value="Cancel" @click="cancelEdit">
+                    </div>
                 </div>
 
-                <div class="h-full w-2/5 flex items-center justify-center space-x-[5%]">
-                    <input type="text" class="field w-3/5 text-center" id="label_editted" v-model="label_editted"
-                        v-on:keydown.enter.prevent='addLabel'>
-                    <input class="button w-[30%]" id="addLabelButton" type="button" value="Add Label"
-                        v-on:click="addLabel">
-
+                <div v-else class="space-x-4 h-[10%] flex items-center">
+                    <div class="text-center w-full space-x-[1%]">
+                        <input type="button" class="button" value="Edit Item" @click="startEdit">
+                        <input type="button" class="button" value="Delete Item" @click="deleteItem">
+                    </div>
                 </div>
-
-            </div>
-
-            <div v-else class="h-[55%] w-[100%] items-center justify-center">
-                <div class="flex items-center justify-center">
-                    <h1 class="text-center text-2xl">item Labels:</h1>
-                </div>
-                <div class="h-[80%] black-bg overflow-auto">
-                    <div class=" mb-4 mx-[1%]" v-for="(lb, index) in item.labels"><span
-                            class="label-pure-black">{{lb}}</span></div>
-                </div>
-            </div>
-
-            <div v-if="editting" class="space-x-4 h-[10%] flex items-center">
-                <div class="text-center w-full space-x-[1%]">
-                    <input type="button" class="button" value="Save" @click="saveEdit">
-                    <input type="button" class="button" value="Cancel" @click="cancelEdit">
-                </div>
-            </div>
-
-            <div v-else class="space-x-4 h-[10%] flex items-center">
-                <div class="text-center w-full space-x-[1%]">
-                    <input type="button" class="button" value="Edit Item" @click="startEdit">
-                    <input type="button" class="button" value="Delete Item" @click="deleteItem">
-                </div>
-            </div>
+            </transition>
         </div>
     </div>
 
